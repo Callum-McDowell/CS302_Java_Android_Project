68950263c98b83743fc28221f01eb5fa8deb19d6
CallumMcDowell <68797974+CallumMcDowell@users.noreply.github.com>
2021-05-31 21:34:56 +1200

    Working ArrayAdapter<Item>

--- COMPSYS302_Project_Two/app/src/main/java/com/example/compsys302_project_two/MainActivity.java
+++ COMPSYS302_Project_Two/app/src/main/java/com/example/compsys302_project_two/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.compsys302_project_two;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // TEMP for TESTING
+        List<ItemDetail> listDetails = DataProvider.getItems();
+        List<Item> list = new ArrayList<Item>(listDetails);
+
+        ItemAdapter itemsAdapter = new ItemAdapter(this,R.layout.list_item, list);
+
+        ListView list_id = (ListView) findViewById(R.id.list);
+        list_id.setAdapter(itemsAdapter);
     }
 }
