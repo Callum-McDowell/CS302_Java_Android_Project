8699992f2f46556d4f42db762940070d4ed9f56f
uoa-hlin784 <hlin784@aucklanduni.ac.nz>
2021-05-31 16:18:27 +1200

    Migrated UI work to COMPSYS302_Project_Two

--- COMPSYS302_Project_Two/app/src/main/java/com/example/compsys302_project_two/MainActivity.java
+++ COMPSYS302_Project_Two/app/src/main/java/com/example/compsys302_project_two/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.compsys302_project_two;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // create an action bar button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // R.menu.* is a reference to a menu.xml file in the res/menu directory.
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void updateTopPicks (View view) {
+
     }
 }
