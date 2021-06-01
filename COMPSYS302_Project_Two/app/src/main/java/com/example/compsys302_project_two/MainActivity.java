a27e40c8806d5f93d0d2a985722f9ca792c3bb09
uoa-hlin784 <hlin784@aucklanduni.ac.nz>
2021-06-01 15:08:24 +1200

    Added searchview
    - Performed id refactoring.
    - Linked SOME icon to change actions for debugging purposes

--- COMPSYS302_Project_Two/app/src/main/java/com/example/compsys302_project_two/MainActivity.java
+++ COMPSYS302_Project_Two/app/src/main/java/com/example/compsys302_project_two/MainActivity.java
@@ -16,6 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bot_nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected (MenuItem item){
+                        switch (item.getItemId()) {
+                            case R.id.bot_nav_search:
+                                startSearchActivity(item);
+                                return true;
+                            case R.id.bot_nav_home:
+                                // do something here
+                                return true;
+                            case R.id.bot_nav_abouts:
+                                // do something here
+                                return true;
+                            default:
+                                return true;
+                        }
+                    }
+                });
     }
 
     // create an action bar button
@@ -23,11 +44,21 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // R.menu.* is a reference to a menu.xml file in the res/menu directory.
         // This is for action bar actions
-        getMenuInflater().inflate(R.menu.search_menu, menu);
+        getMenuInflater().inflate(R.menu.action_bar_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
-    // Placeholder for update icon
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_search) {
+            startSearchActivity(item);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+        // Placeholder for update icon
     public void updateTopPicks (View view) {
 
     }
@@ -35,6 +66,12 @@ public class MainActivity extends AppCompatActivity {
     // Placeholder method for debugging details activity (access through actionbar icon)
     public boolean openDetails (MenuItem item) {
         Intent intent = new Intent(this, DetailsActivity.class);
+        startActivity(intent);
+        return true;
+    }
+
+    public boolean startSearchActivity (MenuItem item) {
+        Intent intent = new Intent(this, SearchActivity.class);
         startActivity(intent);
         return true;
     }
