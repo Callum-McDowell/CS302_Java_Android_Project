24c85d3f8ceea3361540bf8f75fe2dc42c2bd2b3
uoa-hlin784 <hlin784@aucklanduni.ac.nz>
2021-06-05 16:52:18 +1200

    Refactored SplashActivity

--- COMPSYS302_Project_Two/app/src/main/java/com/example/compsys302_project_two/SplashActivity.java
+++ COMPSYS302_Project_Two/app/src/main/java/com/example/compsys302_project_two/SplashActivity.java
@@ -16,6 +16,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.text.Spannable;
 import android.text.SpannableString;
+import android.transition.Explode;
 import android.transition.Fade;
 
 import android.app.ActivityOptions;
@@ -34,26 +35,23 @@ import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private ViewGroup splashLayout;
-    private TextView splashTitle;
-    private View background;
-    private View featureText;
+    class ViewHolder {
+        private TextView splashTitle;
+    }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    ViewHolder vh = new ViewHolder();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setupTransition();
         setContentView(R.layout.activity_splash);
 
-//        splashLayout = (ViewGroup) findViewById(R.id.splash_layout);
-        splashTitle = (TextView) findViewById(R.id.splash_title);
-//        background = (View) findViewById(R.id.splash_background);
-//        featureText = (View) findViewById(R.id.splash_feature_text);
+        vh.splashTitle = (TextView) findViewById(R.id.splash_title);
         setSplashTitle("Garden Hub");
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setupTransition();
+        }
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -64,20 +62,20 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     // Setup activity transition settings
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void setupTransition() {
-        Transition transition = new Fade();
-        transition.setDuration(3000);
-//        getWindow().setEnterTransition(transition);
-        getWindow().setExitTransition(transition);
-//        TransitionManager.beginDelayedTransition(splashLayout, transition);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Transition transition = new Fade();
+            transition.setDuration(3000);
+            getWindow().setExitTransition(transition);
+        }
     }
 
     public void startMainActivity() {
         Intent intent = new Intent(getBaseContext(), MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 
+        // Choice between starting with animated transition vs no animated transition
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(SplashActivity.this).toBundle());
         } else {
             startActivity(intent);
@@ -90,10 +88,10 @@ public class SplashActivity extends AppCompatActivity {
         SpannableString s = new SpannableString(title);
         s.setSpan(new TypefaceSpan(this, "lehavre_roughbasic.otf"), 0, s.length(),
                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        splashTitle.setText(s);
+        vh.splashTitle.setText(s);
     }
 
-    // Override onStop() to destory splashscreen a bit later to allow connection with MainActivity's
+    // Override onStop() to destroy splashscreen a bit later to allow connection with MainActivity's
     // transition animation
     @Override
     public void onStop() {
